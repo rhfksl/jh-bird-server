73052feab725117a104212536bc0156cd0e45f89
rhfksl <wkaqkdrn@gmail.com>
2020-06-08 15:18:33 +0900

    ✨ socket io 도입
     - 소켓 아이오 적용 실시간 채팅
     - User model 연결을 통해 첫 입장시 해당 채팅 내용 얻어와 emit
     - 새로운 메세지 전달 받았을 시 db에 저장 후 클라이언트에 다시 보냄

---
+++ controllers/userController/userController.js
@@ -0,0 +1,28 @@
+const { Users } = require('../../models');
+
+const postUser = (req, res) => {
+  // Find or create the game in the games table
+  // Register the score along with the given user_id using JWT token
+  const { userId, password, nickname } = req.body;
+
+  Users.findOrCreate({
+    where: { userId },
+    defaults: {
+      password,
+      nickname,
+    },
+  })
+    // eslint-disable-next-line
+    .then(([result, created]) => {
+      if (!created) {
+        res.sendStatus(409);
+      } else {
+        res.sendStatus(201);
+      }
+    })
+    .catch(() => {
+      res.sendStatus(409);
+    });
+};
+
+module.exports = postUser;
--- models/users.js
+++ models/users.js
@@ -1,54 +1,28 @@
 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   const Users = sequelize.define('Users', {
-//     userId: DataTypes.STRING,
-//     password: DataTypes.STRING,
-//     nickname: DataTypes.STRING
-//   }, {});
-//   Users.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return Users;
-// };
 
 const Sequelize = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define(
-    'Users',
-    {
-      userId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      nickname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    }
-    // {
-    //   timestamps: false,
-    //   hooks: {
-    //     afterValidate: (user) => {
-    //       // eslint-disable-next-line
-    //       user.password = encrypt(user.password);
-    //     },
-    //   },
-    // },
-  );
+  const Users = sequelize.define('Users', {
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
   // eslint-disable-next-line
   Users.associate = (models) => {
-    // Users.hasMany(models.ChattingRoom);
-    // Users.hasOne(models.FriendList);
-    // Users.hasOne(models.FriendList);
     Users.hasMany(models.ChattingRoom, { foreignKey: 'user_id' });
     Users.hasOne(models.FriendList, { foreignKey: 'user_id' });
     Users.hasOne(models.FriendList, { foreignKey: 'friend_id' });
-    Users.hasMany(modesl.Chat, { foreignKey: 'user_id' });
+    Users.hasMany(models.Chat, { foreignKey: 'user_id' });
   };
   return Users;
 };
